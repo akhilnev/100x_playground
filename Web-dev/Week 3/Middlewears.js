e5478eb508a226c07f7d0f2e5bca4337e0712d4e
Akhilesh Nevatia <akhilesh.nevatia@gmail.com>
2023-12-26 19:24:07 +0530

    Writing notes on Zod in Middlewears.js and Notes.txt

--- Web-dev/Week 3/Middlewears.js
+++ Web-dev/Week 3/Middlewears.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const z = require("zod"); // also do <npm install zod> to get the zod library onto local machine!
+
 
 const app = express();
 
@@ -48,5 +50,38 @@ app.get("/healthcheckup",usercheckMiddleware,kidneycheckMiddleware,(req,res)=>{
     res.status(200).send("You are healthy !!");
 });
 
+// SAY I WANT TO USE A MIDDLEWEAR IN ALL FUNCTIONS BELOW SOMETHING I WILL WRITEE
+
+app.use(usercheckMiddleware); // this will run the middleware on all routes below it !!
+
+app.use(express.json()); // this is a middleware to handle the body of the request, as we dont know body can be text,HTML,JSON,etc and this function allows us to do direct req.body instead of parsing or figuring out what to parse !!
+// USUALLY DONE ON THE TOP^^^
+
+
+
+// INPUT VALIDATION - USERS CAN SEND RANDOM SHIT TO BREAK BACKEND AND TRY TO BREAK THE BACKEND BUT THAT NEEDS TO BE REGULATED - MANUAL INDUVIDUAL EDGE CASE HANDLONG OR ZOD LIBRARY FOR INPUT CHECKS CAN BE USED !!
+
+
+const mySchema = z.array(z.number); // desired type for kidneys check!!
+
+app.post("/health-checkup",function(req,res){
+    //kidneys = [1,2,3] - example of a valid input
+    const kidneys = req.body.kidneyId;
+    const response = mySchema.safeParse(kidneys);
+    res.send({response}); // AS SAFE PARSE SENDS A JSON!!
+    const kidneyLength = kidneys.length;
+
+    res.send("You have " + kidneyLength + " kidneys !!");
+})
+
+const schema = z.object({
+    email : z.string().email(), // email checker 
+    password: app.string.min(5), // min 5 length 
+    kidneyId: z.literal(1).or.literal(2) // exactly 1 or 2
+});
+// full schema checks for ful req.body in 1 go!!
+
+
+app.use(function(err,req,res,next){res.status(500).json({msg:"Sorry something is up"})}) // GLOBAL CACHES!! 
 
 app.listen(3000,function(){console.log("Server started !!")});
