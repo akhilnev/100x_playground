cc77c1e5762f49952e911d0e57b9961d343625d9
Akhilesh Nevatia <akhilesh.nevatia@gmail.com>
2024-01-22 13:53:24 -0600

    Hitting local backend using cors plug-in in text file

--- Web-dev/Week4/sum-server.js
+++ Web-dev/Week4/sum-server.js
@@ -1,12 +1,8 @@
-express = require("express");
+const cors = require('cors');
+const express = require('express');
 const app = express();
+app.use(cors());
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'file:///Users/akhileshnevatia/Zerdha_Basic_App/100x_playground/Web-dev/Week4/2box.html'); // Replace with your frontend domain
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
 
 app.get("/sum", function(req,res){
     const a = parseInt(req.query.a);
